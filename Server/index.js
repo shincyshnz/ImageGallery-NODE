4936fcd6087e55a3fff336ae1da3d49ffaede250
Shincy <shincyshnz@gmail.com>
2023-06-01 10:14:57 +0530

    Middleware-APplicationa nd specific implemented with express router

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-const multer = require("multer");
-const fs = require("fs");
 const app = express();
-const { checkAuth } = require("./middlewares/checkAuth");
+
+const ImageGalleryRoute = require('./routes/imageGallery');
 
 app.use(cors());
 app.use(express.json());
@@ -14,7 +13,6 @@ const userData = [];
 
 
 //Application Level middleware
-// const logStuff = [logData]
 app.use((req, res, next) => {
     const newData = {
         "url": req.url,
@@ -27,47 +25,13 @@ app.use((req, res, next) => {
     next();
 });
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        //specify the directory where you want to store uploaded file
-        cb(null, "public/images");
-    },
-    filename: function (req, file, cb) {
-        // Generate a unique name for the uploaded file
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        const extension = file.originalname.split(".").pop();
-        cb(null, file.fieldname + '-' + uniqueSuffix + '.' + extension);
-    }
-});
-const upload = multer({ storage: storage });
-
-app.get("/", (req, res) => {
-    // read images folder
-    fs.readdir("public/images", (err, files) => {
-        if (err) {
-            res.status(400).json(err);
-            return;
-        };
-        res.status(200).json(files);
-    })
-});
-
-app.post("/upload", checkAuth, upload.array("upload-files", 12), (req, res) => {
-    if (req.files.length > 0) {
-        const files = req.files.map((file) => {
-            return file.filename
-        });
-        res.status(200).json({ files });
-    }
-});
-
+app.use("/", ImageGalleryRoute);
 
 app.use("*", (req, res) => {
     res.status(404).json({
         message: "This Route does not exist",
     });
 });
-
 
 
 const PORT = 3008;
--- Server/middlewares/checkAuth.js
+++ Server/middlewares/checkAuth.js
@@ -1,13 +1,11 @@
 const checkAuth = (req, res, next) => {
     const { auth } = req.body;
-    // const { auth } = req.files;
-    console.log(req.body.auth, "===auth");
-    console.log(req.files, "===req");
     if (!auth) {
         return res.status(401).json({
             message: "un-authorized"
         });
     }
+    console.log("----------Inside Specific middleware : CheckAuth--------");
     next();
 }
 
